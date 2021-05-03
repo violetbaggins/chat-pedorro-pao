d9a1e3e9720859fdbf3a70cda92b759df5db8c90
violetbaggins <violetbaggins@gmail.com>
2021-05-02 22:45:48 -0300

    empece users

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages')
 
 const app = express();
 const server = http.createServer(app)
@@ -10,25 +11,26 @@ const io = socketio(server)
 // set static
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = "chatbot"
 
 // run when a client connects
 io.on('connection', socket => {
     // console.log('web socket connection 3');
 
     // solo al user q se loguea
-    socket.emit('message', 'Welcome to Jurassic Park');
+    socket.emit('message', formatMessage("bot bienvenida", 'Welcome to Jurassic Park'));
 
     // broadcast when user connects
-    socket.broadcast.emit('message', 'user joined chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'user joined chat'));
 
     // broadcast when user disconnects
     socket.on('disconnect', ()=>{
-        io.emit('message', 'user left chat')
+        io.emit('message', formatMessage(botName, 'user left chat'))
     }  );
 
     // listen for chat msg
     socket.on('chatMessage', msg =>{
-        io.emit('message', msg);
+        io.emit('message',formatMessage('USER', msg));
     })
 
     
